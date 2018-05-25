51dd6d7c371f8814243947694960f9e2210eecff
Edmel John Ricahuerta <ejricahuerta@sy-s2114-110.local>
2018-05-24 21:59:29 -0400

    added implementation of Calculator

---
+++ Calculator/Calculator/Calculator_Brain.h
@@ -0,0 +1,13 @@
+//
+//  Calculator_Brain.h
+//  Calculator
+//
+//  Created by Edmel John Ricahuerta on 2018-05-24.
+//  Copyright © 2018 com.map523. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator_Brain : NSObject
+
+@end
---
+++ Calculator/Calculator/Calculator_Brain.m
@@ -0,0 +1,13 @@
+//
+//  Calculator_Brain.m
+//  Calculator
+//
+//  Created by Edmel John Ricahuerta on 2018-05-24.
+//  Copyright © 2018 com.map523. All rights reserved.
+//
+
+#import "Calculator_Brain.h"
+
+@implementation Calculator_Brain
+
+@end
