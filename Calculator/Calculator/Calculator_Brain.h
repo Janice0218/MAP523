881a41041afa4e971aa90a26fb61208218a3f62d
Edmel Ricahuerta <exricahuerta@gmail.com>
2018-05-25 16:26:11 -0400

    update via web

--- Calculator/Calculator/Calculator_Brain.h
+++ Calculator/Calculator/Calculator_Brain.h
@@ -2,12 +2,19 @@
 //  Calculator_Brain.h
 //  Calculator
 //
-//  Created by Edmel John Ricahuerta on 2018-05-24.
+//  Created by Edmel John Ricahuerta on 2018-05-25.
 //  Copyright © 2018 com.map523. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface Calculator_Brain : NSObject
+
+@property (nonatomic,strong) NSMutableArray* numbers;
+
+-(void) pushItem: (double) number;
+-(double) calculate : (NSString *) operation;
+
+
 
 @end
