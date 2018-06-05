46045d03c155c15b0b23083f8499db0671d24019
Edmel John Ricahuerta <ejricahuerta@sy-sycc-13.local>
2018-06-05 14:57:41 -0400

    working UI updated.

--- Calculator/Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/Calculator/ViewController.m
@@ -13,14 +13,15 @@
 
 @property(strong,nonatomic) Calculator_Brain * cal;
 @property (weak, nonatomic) IBOutlet UILabel *numberOutput;
-
+@property BOOL numberEntered;
 
 @end
 
 @implementation ViewController
+@synthesize cal = _cal;
 
 //getter
--(Calculator_Brain*) cal{
+-(Calculator_Brain* ) cal{
     if(_cal ==nil){
         _cal = [[Calculator_Brain alloc]init];
     }
@@ -29,32 +30,47 @@
 
 //events
 - (IBAction)numberClick:(UIButton *)sender {
-    _numberOutput.text =  sender.currentTitle;
+    _numberOutput.text = [NSString stringWithFormat:@"%@%@",_numberOutput.text,sender.currentTitle];
 }
 
-
 - (IBAction)operationClick:(UIButton *)sender {
-    if([sender.currentTitle isEqualToString:@"//"]){
-        
+    if([sender.currentTitle isEqualToString:@"/"]){
+        double output = [_cal calculate:@"/"];
+      _numberOutput.text =
+        [[NSString alloc]initWithFormat:@"%.2f" ,output];
     }
     else if([sender.currentTitle isEqualToString:@"*"]){
-        
+        double output = [_cal calculate:@"*"];
+        _numberOutput.text =
+        [[NSString alloc]initWithFormat:@"%.2f" ,output];
     }
     else if([sender.currentTitle isEqualToString:@"-"]){
-        
+        double output = [_cal calculate:@"-"];
+        _numberOutput.text =
+        [[NSString alloc]initWithFormat:@"%.2f" ,output];
     }
     else if([sender.currentTitle isEqualToString:@"+"]){
-        
+        double output = [_cal calculate:@"+"];
+        _numberOutput.text =
+        [[NSString alloc]initWithFormat:@"%.2f" ,output];
     }
+    _numberEntered = true;
     
 }
 - (IBAction)enterNumber:(id)sender {
-    
+    if(_cal ==nil){
+        _cal = [[Calculator_Brain alloc]init];
+        
+    }
+    double val = [_numberOutput.text doubleValue];
+    [_cal pushItem: val];
+       _numberOutput.text = @"";
+
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    _numberEntered = false;
     // Do any additional setup after loading the view, typically from a nib.
     
     
