bcf4ac4a0e5bbf76ff037e0f7bfc0e88bec53433
Edmel John Ricahuerta <ejricahuerta@sy-sycc-04.local>
2018-08-11 19:28:06 -0400

    override viewwilldissappear

--- StockWatch/StockWatch/StockDetailScreen.swift
+++ StockWatch/StockWatch/StockDetailScreen.swift
@@ -9,15 +9,23 @@
 import UIKit
 
 class StockDetailScreen: UIViewController {
-    
-    
+
+
     
     var symbol : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.title = symbol
+        self.navigationItem.title = symbol
+        
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        let main  = navigationController?.topViewController as! MainScreen
+        main.tableView.reloadData()
+    }
+    @IBAction func reloadTapped(_ sender: UIBarButtonItem) {
+    }
+
 }
 
 
@@ -25,11 +33,11 @@ extension StockDetailScreen : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! DetailCell
-        cell.openDetail.text    = "open         : "
-        cell.closeDetail.text   = "close        : "
-        cell.highDetail.text    = "high         : "
-        cell.lowDetail.text     = "low            : "
-        cell.volumeDetail.text  = "volume     : "
+        cell.openDetail.text    = "open"
+        cell.closeDetail.text   = "close"
+        cell.highDetail.text    = "high"
+        cell.lowDetail.text     = "low"
+        cell.volumeDetail.text  = "volume"
         
         return cell
         
@@ -37,6 +45,9 @@ extension StockDetailScreen : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\tInterval: 1 min"
     }
 }
 
