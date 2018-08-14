9939becb64363b191cef0a816fe40f7fb6952881
Edmel John Ricahuerta <ejricahuerta@sy-sycc-01.local>
2018-08-14 00:19:03 -0400

    moved error to own file

---
+++ StockWatch/StockWatch/Error.swift
@@ -0,0 +1,15 @@
+//
+//  Error.swift
+//  StockWatch
+//
+//  Created by Edmel John Ricahuerta on 2018-08-13.
+//  Copyright © 2018 com.senecac.map. All rights reserved.
+//
+
+import Foundation
+
+enum StockError : Error {
+    case notFound
+    case emptyStock
+    case intervalError
+}
