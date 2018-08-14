beef8eef3a367aef494996e8b2e3a81a1e3a8727
Edmel John Ricahuerta <ejricahuerta@sy-sycc-01.local>
2018-08-14 00:20:26 -0400

    add comment

--- StockWatch/StockWatch/StockDetailScreen.swift
+++ StockWatch/StockWatch/StockDetailScreen.swift
@@ -12,49 +12,34 @@ class StockDetailScreen: UIViewController {
     
     @IBOutlet weak var tableVIew: UITableView!
     @IBOutlet weak var loadingItem: UIActivityIndicatorView!
+    
+    
+    
+    //symbol used for this view
     var symbol : String = ""
     
+    //stock manager reference
     var stockManager : StockManager?
+    
+    //all stocks OHLC details
     var details : [StockOHLCModel] = []
     
     
     
+    //
+    //  load init data
+    //
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         self.navigationItem.title = symbol
-        
-        self.loadingItem.isHidden = false
-        self.loadingItem.startAnimating()
-        
-        let search = symbol.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        
-        self.stockManager?.listStockDetailsBy(symbol: search, handler: { (all, error) in
-            
-            self.details.removeAll()
-        
-            DispatchQueue.main.async {
-                
-                if all == nil  || all?.count == 0 || error != nil {
-                    
-                    let errorMessage = error.unsafelyUnwrapped.localizedDescription
-                    let alertController = UIAlertController(title: "Result Empty", message:
-                        "\(String(describing:errorMessage ))", preferredStyle: UIAlertControllerStyle.alert)
-                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler:  { action in
-                        self.navigationController?.popViewController(animated: true)
-                    }))
-                    self.present(alertController, animated: true, completion: nil)
-                }
-                else {
-                    self.details = all!
-                    self.tableVIew.reloadData()
-                    self.loadingItem.stopAnimating()
-                    self.loadingItem.isHidden = true
-                }
-            }
-        })
+        dataDidLoad()
         
     }
+    
+    
+    //
+    //  reload the table for main screen when this view disappear
+    //
     override func viewWillDisappear(_ animated: Bool) {
         
         let main  = navigationController?.topViewController as! MainScreen
@@ -62,30 +47,19 @@ class StockDetailScreen: UIViewController {
         
     }
     
-    
+    //
+    //  reload data when tapped
+    //
     @IBAction func reloadTapped(_ sender: UIBarButtonItem) {
-        
-        
-        self.loadingItem.isHidden = false
-        self.loadingItem.startAnimating()
-        
-        let search = symbol.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        
-        self.stockManager?.listStockDetailsBy(symbol: search, handler: { (all,error) in
-            self.details.removeAll()
-            self.details = all!
-            DispatchQueue.main.async {
-                self.tableVIew.reloadData()
-                self.loadingItem.stopAnimating()
-                self.loadingItem.isHidden = true
-            }
-        })
+       dataDidLoad()
     }
 }
 
 extension StockDetailScreen : UITableViewDelegate, UITableViewDataSource {
     
-    //table methods
+    //
+    //  Populate the table with data from Resource
+    //
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! DetailCell
         
@@ -98,17 +72,76 @@ extension StockDetailScreen : UITableViewDelegate, UITableViewDataSource {
         cell.lowDetail.text     = "\(allDetails[indexPath.row].low)"
         cell.volumeDetail.text  = "\(allDetails[indexPath.row].volume)"
         return cell
-        
     }
     
+    
+    //
+    //  Declaring the numbers of row for tableview
+    //
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return details.count
     }
+    
+    
+    
+    //
+    //  Renaming the section title
+    //
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "\tInterval:\(stockInterval) Min"
+        return "\tInterval:\(stockInterval) Minute"
     }
     
     
+    
+    //
+    //  This function loads the data to table view from stockManager
+    //  An alert Message is called when any error occur
+    //
+    func dataDidLoad()->Void {
+        
+        self.loadingItem.isHidden = false
+        self.loadingItem.startAnimating()
+        
+        self.stockManager?.listStockDetailsBy(symbol: symbol, handler: { (all, error) in
+            
+            //  remove all details data
+            self.details.removeAll()
+            
+            //return to main queue
+            DispatchQueue.main.async {
+                
+                //check if there are not data from list or as well as possible errors
+                if all == nil  || all?.count == 0 || error != nil {
+                    
+                    
+                    //  unwrapped error to string
+                    let errorMessage = error.unsafelyUnwrapped.localizedDescription
+                    
+                    //  create instance for alert view
+                    let alertController = UIAlertController(title: "Result Empty", message:
+                        "\(String(describing:errorMessage ))", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    //  add action for alert
+                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler:  { action in
+                        self.navigationController?.popViewController(animated: true)
+                    }))
+                    
+                    //  present alert
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                    
+                    
+                else { // if data exist and no errors
+                    self.details = all!
+                    self.tableVIew.reloadData()
+                    self.loadingItem.stopAnimating()
+                    self.loadingItem.isHidden = true
+                }
+            }
+        })
+        
+        
+    }
 }
 
 
