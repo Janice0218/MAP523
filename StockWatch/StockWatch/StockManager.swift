202afe471628a22f14e8c273365c1838b72e8556
Edmel John Ricahuerta <ejricahuerta@sy-sycc-01.local>
2018-08-14 00:19:26 -0400

    add comment

--- StockWatch/StockWatch/StockManager.swift
+++ StockWatch/StockWatch/StockManager.swift
@@ -16,26 +16,34 @@ import UIKit
 
 class StockManager {
     
+    
+    //  instance for db
     private var dbManager = DataManager()
+    
+    //  instance for request data fetcher
     private var requestData = RequestDataFetcher()
     private var AllStockFromDb  = [Stock]()
     
-    //injectin DB into the stockManager
+    //  injectin DB into the stockManager
+    //  get init data
     init(db : DataManager) {
         self.dbManager = db
         AllStockFromDb = dbManager.fetchAll()
         
     }
     
+    //
+    //  list data from db
+    //
     func listStocksfromDb() -> [Stock] {
         AllStockFromDb = dbManager.fetchAll()
         return AllStockFromDb
     }
     
-    /*
-     This function list stock OHLC from symbol passed
-     that must exist in the data store.
-     */
+    //
+    //  This function list stock OHLC from symbol passed
+    //  that must exist in the data store.
+    //
     func listStocksBy(query : String, handler : @escaping (_ all : [StockModel])-> Void) {
         
         let stringUrl  = "\(yahoohost)\(query)&\(yahooregion)&\(yahoolanguage)&\(yahoocallback)"
@@ -53,29 +61,38 @@ class StockManager {
         }
     }
     
-    /*
-     This function list stock OHLC from symbol passed
-     that must exist in the data store.
-     */
+    //
+    //  This function list stock OHLC from symbol passed
+    //  that must exist in the data store.
+    //
     func listStockDetailsBy(symbol : String, handler : @escaping (_ all : [StockOHLCModel]?, _ error : Error? )-> Void) {
         
+        // url for detail
         let detail =  SymbolDetailURL.init(host: stockhost, function: stockfunc, symbol: symbol, interval: stockInterval, apiKey: stockApiKey)
         
+        //  get url from detail
         let stringUrl = detail.url
+        
+        //  pass string as URL
         if let url = URL(string : stringUrl) {
+            
+            //  get data from request data fetcher call
             self.requestData.getData(url: url, forKey: "Time%Series%(1min)") { (result) in
-                switch result {
-                case .success(let details):
-                    handler(details as? [StockOHLCModel], nil)
-                case .failure(let error):
-                    handler(nil, error)
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let details):
+                        handler(details as? [StockOHLCModel], nil)
+                    case .failure(let error):
+                        handler(nil, error)
+                    }
                 }
             }
         }
     }
     
-    
-    //needs refactor
+    //
+    //  add data to DB
+    //
     func AddStockToDb(stock : StockModel)->Void {
         dbManager.AddEntity(name: stock.Name,
                             symbol: stock.Symbol,
@@ -85,7 +102,9 @@ class StockManager {
                             typeDisp: stock.TypeDisp)
     }
     
-    
+    //
+    //  remove data
+    //
     func removeStockBy(symbol : String){
         AllStockFromDb = AllStockFromDb.filter({ (stock) -> Bool in
             return stock.symbol != symbol
