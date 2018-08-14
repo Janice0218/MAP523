8f2116d13d0767002302b19b65575e0bd5c45178
Edmel John Ricahuerta <ejricahuerta@sy-sycc-01.local>
2018-08-13 20:12:56 -0400

    handler and stock crud add

--- StockWatch/StockWatch/StockManager.swift
+++ StockWatch/StockWatch/StockManager.swift
@@ -18,17 +18,21 @@ class StockManager {
     
     private var dbManager = DataManager()
     private var requestData = RequestDataFetcher()
-    private var AllStocks = [StockModel]()
-
+    private var AllStockFromDb  = [Stock]()
     
     //injectin DB into the stockManager
     init(db : DataManager) {
         self.dbManager = db
+        AllStockFromDb = dbManager.fetchAll()
+       
     }
     
-    
     func listStocksfromDb() -> [Stock] {
-        return dbManager.fetchAll()
+        let fetched = dbManager.fetchAll()
+        if fetched.count > AllStockFromDb.count {
+            AllStockFromDb = fetched
+        }
+        return AllStockFromDb
     }
     
     /*
@@ -39,35 +43,35 @@ class StockManager {
         
         let stringUrl  = "\(yahoohost)\(query)&\(yahooregion)&\(yahoolanguage)&\(yahoocallback)"
         if let url = URL(string : stringUrl) {
-                self.requestData.getData(url: url, forKey: "ResultSet.Result") { (result) in
+            self.requestData.getData(url: url, forKey: "ResultSet.Result") { (result) in
+                DispatchQueue.main.async {
                     switch result {
                     case .success(let stocks):
-                        DispatchQueue.main.async {
-                            handler(stocks as! [StockModel])
-                        }
+                        handler(stocks as! [StockModel])
                     case .failure(let error):
                         print("ERROR: \(error.localizedDescription)")
                     }
+                }
             }
         }
     }
     
     /*
-        This function list stock OHLC from symbol passed
-        that must exist in the data store.
+     This function list stock OHLC from symbol passed
+     that must exist in the data store.
      */
-    func listStockDetailsBy(symbol : String, handler : @escaping (_ all : [StockOHLCModel])-> Void) {
-
+    func listStockDetailsBy(symbol : String, handler : @escaping (_ all : [StockOHLCModel]?, _ error : Error? )-> Void) {
+        
         let detail =  SymbolDetailURL.init(host: stockhost, function: stockfunc, symbol: symbol, interval: stockInterval, apiKey: stockApiKey)
         
-         let stringUrl = detail.url
+        let stringUrl = detail.url
         if let url = URL(string : stringUrl) {
             self.requestData.getData(url: url, forKey: "Time%Series%(1min)") { (result) in
                 switch result {
                 case .success(let details):
-                        handler(details as! [StockOHLCModel])
+                    handler(details as? [StockOHLCModel], nil)
                 case .failure(let error):
-                    print("ERROR: \(error.localizedDescription)")
+                    handler(nil, error)
                 }
             }
         }
@@ -76,23 +80,27 @@ class StockManager {
     
     //needs refactor
     func AddStockToDb(stock : StockModel)->Void {
-         dbManager.AddEntity(name: stock.Name,
-                             symbol: stock.Symbol,
-                             exch: stock.Exch,
-                             exchDisp: stock.ExchDisp,
-                             type: stock.JsonType,
-                             typeDisp: stock.TypeDisp)
-    }
-    
-    
-    func removeStockBy(symbol : String)-> Void {
         
         
-       let index =  AllStocks.map { (s) -> Int in
-                s.Symbol == symbol
+        let count  = AllStockFromDb.filter( { (stockInDB) -> Bool in
+             return stockInDB.symbol == stock.Symbol
+            }).count
+        if count < 0 {
+            dbManager.AddEntity(name: stock.Name,
+                                symbol: stock.Symbol,
+                                exch: stock.Exch,
+                                exchDisp: stock.ExchDisp,
+                                type: stock.JsonType,
+                                typeDisp: stock.TypeDisp)
         }
     }
     
+    
+    func removeStockBy(symbol : String){
+        AllStockFromDb = AllStockFromDb.filter({ (stock) -> Bool in
+            return stock.symbol != symbol
+        })
+    }
 }
 
 
