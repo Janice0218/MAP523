71d3b4298b78621adf95c3fc6ae8db1a1e36e1de
Edmel John Ricahuerta <ejricahuerta@sy-sycc-15.local>
2018-08-14 23:26:41 -0400

    whitespace

--- StockWatch/StockWatch/RequestDataFetcher.swift
+++ StockWatch/StockWatch/RequestDataFetcher.swift
@@ -62,7 +62,6 @@ class RequestDataFetcher {
                     }
                 }
                 
-                
                 do {
                     
                     //  serialize data
