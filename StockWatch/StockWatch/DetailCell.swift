bdcef6eabe16af816ddea0b43f224aadbb18fa21
Edmel John Ricahuerta <ejricahuerta@sy-sycc-15.local>
2018-08-14 21:37:44 -0400

    setter for cell labels

--- StockWatch/StockWatch/DetailCell.swift
+++ StockWatch/StockWatch/DetailCell.swift
@@ -13,16 +13,19 @@ class DetailCell: UITableViewCell {
     //Custom Cell
 
     @IBOutlet weak var timeDetail: UILabel!
-    
     @IBOutlet weak var openDetail: UILabel!
-    
     @IBOutlet weak var closeDetail: UILabel!
-    
     @IBOutlet weak var highDetail: UILabel!
-    
     @IBOutlet weak var lowDetail: UILabel!
-    
     @IBOutlet weak var volumeDetail: UILabel!
-
+    
+    
+    func setLabels(time: String, open : Double,high : Double , low : Double, close :Double ) {
+        self.timeDetail.text    =   "\(time )"
+        self.openDetail.text    =   "\(open )"
+        self.highDetail.text    =   "\(high )"
+        self.lowDetail.text     =   "\(low  )"
+        self.closeDetail.text   =   "\(close)"
+    }
 
 }
