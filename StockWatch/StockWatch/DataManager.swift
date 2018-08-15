642809151940ca284aea8bbb74f9fe5572e74c6d
Edmel John Ricahuerta <ejricahuerta@sy-sycc-15.local>
2018-08-14 23:27:06 -0400

    add delete crud function

--- StockWatch/StockWatch/DataManager.swift
+++ StockWatch/StockWatch/DataManager.swift
@@ -13,7 +13,6 @@ import UIKit
 
 class DataManager {
     
-    
     //  reference app delegate pointer
     var appDelegatePointer = UIApplication.shared.delegate as! AppDelegate
     
@@ -21,19 +20,19 @@ class DataManager {
     var allData = [Stock]()
     
     
+    let fetchrequestForStock : NSFetchRequest<Stock> = Stock.fetchRequest()
     //
     //  fetch all data from core data
     //
     func fetchAll() -> [Stock] {
-        //determine data to fetch
-        let fetchrequest : NSFetchRequest<Stock> = Stock.fetchRequest()
         
         do {
             //grab data
-            let stocks = try appDelegatePointer.persistentContainer.viewContext.fetch(fetchrequest)
+            let stocks = try appDelegatePointer.persistentContainer.viewContext.fetch(fetchrequestForStock)
             
             if allData.count != stocks.count {
                 allData.removeAll()
+                
                 //store data to array
                 for stock in stocks {
                     allData.append(stock)
@@ -43,34 +42,50 @@ class DataManager {
         catch {
             print("Unable to fetch Data")
         }
-        
         return allData
-        
     }
     
-    func AddEntity(name: String ,
-                   symbol :String ,
-                   exch : String ,
-                   exchDisp : String,
-                   type : String,
-                   typeDisp :String)  {
-        
+    func AddEntity(name: String, symbol :String, exch : String, exchDisp : String, type : String, typeDisp :String)  {
         
         let newEntity = NSEntityDescription
             .insertNewObject(forEntityName: "Stock", into: appDelegatePointer.persistentContainer.viewContext)
             as! Stock
         
-        newEntity.name = name
-        newEntity.exchange = exch
-        newEntity.exchangeDisplay = exchDisp
-        newEntity.symbol = symbol
-        newEntity.type = type
-        newEntity.typeDisplay = typeDisp
+        newEntity.name              = name
+        newEntity.exchange          = exch
+        newEntity.exchangeDisplay   = exchDisp
+        newEntity.symbol            = symbol
+        newEntity.type              = type
+        newEntity.typeDisplay       = typeDisp
         
         appDelegatePointer.saveContext()
         
     }
     
-    
+    func deleteEntity(name : String, param : String) {
+        
+        fetchrequestForStock.predicate = NSPredicate.init(format: "symbol==\(param)")
+        
+        do {
+            let object = try appDelegatePointer.persistentContainer.viewContext.fetch(fetchrequestForStock)
+            
+            appDelegatePointer.persistentContainer.performBackgroundTask({ context in
+                let obj : Stock = object[0]
+                do {
+                    context.delete(obj)
+                    
+                        try context.save()
+                }
+                catch {
+                    fatalError()
+                }
+                
+            })
+        }
+        catch {
+            print("Unable to delete from DB")
+        }
+        
+    }
     
 }
